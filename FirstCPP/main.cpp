b88cd4a722cb26d55ad603cb4ca2e725f2dd3221
jyrnan <yoeking@gmail.com>
2022-09-24 15:24:37 +0800

    Initial Commit

---
+++ FirstCPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FirstCPP
+//
+//  Created by jyrnan on 2022/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
